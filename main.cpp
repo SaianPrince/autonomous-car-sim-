6b6ece0ec3aa8c0664d82f39d4c943e5495d2cdf
SaianPrince <faruk2969@hotmail.com>
2026-04-27 15:59:59 +0300

    Implement basic autonomous driving simulation with TCP

---
+++ main.cpp
@@ -0,0 +1,136 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#pragma comment(lib,"ws2_32.lib")
+
+int main()
+{
+    sf::RenderWindow window(
+        sf::VideoMode(800, 600),
+        "Autonomous Driving Simulation"
+    );
+
+    // Vehicle
+    sf::RectangleShape vehicle(
+        sf::Vector2f(50.f, 80.f)
+    );
+
+    vehicle.setFillColor(sf::Color::Red);
+    vehicle.setPosition(375.f, 450.f);
+
+    // Lane markers
+    sf::RectangleShape leftLane(
+        sf::Vector2f(5.f, 600.f)
+    );
+
+    leftLane.setPosition(300.f, 0.f);
+    leftLane.setFillColor(sf::Color::White);
+
+
+    sf::RectangleShape rightLane(
+        sf::Vector2f(5.f, 600.f)
+    );
+
+    rightLane.setPosition(500.f, 0.f);
+    rightLane.setFillColor(sf::Color::White);
+
+
+    // Initialize Winsock
+    WSADATA wsaData;
+    WSAStartup(
+        MAKEWORD(2, 2),
+        &wsaData
+    );
+
+    // Create TCP socket
+    SOCKET clientSocket =
+        socket(
+            AF_INET,
+            SOCK_STREAM,
+            0
+        );
+
+    sockaddr_in serverAddress;
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(5000);
+
+    inet_pton(
+        AF_INET,
+        "127.0.0.1",
+        &serverAddress.sin_addr
+    );
+
+
+    if (connect(
+        clientSocket,
+        (sockaddr*)&serverAddress,
+        sizeof(serverAddress)
+    ) == SOCKET_ERROR)
+    {
+        std::cout << "Connection failed\n";
+        return -1;
+    }
+
+    std::cout << "Connected to Python server\n";
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+        // Send one frame signal
+        const char* frameMessage = "FRAME\n";
+
+        send(
+            clientSocket,
+            frameMessage,
+            strlen(frameMessage),
+            0
+        );
+
+
+        // Receive command
+        char commandBuffer[1024] = {};
+
+        int receivedBytes =
+            recv(
+                clientSocket,
+                commandBuffer,
+                sizeof(commandBuffer) - 1,
+                0
+            );
+
+        if (receivedBytes > 0)
+        {
+            std::cout
+                << "Received command: "
+                << commandBuffer
+                << std::endl;
+        }
+
+
+        window.clear();
+
+        window.draw(leftLane);
+        window.draw(rightLane);
+        window.draw(vehicle);
+
+        window.display();
+    }
+
+
+    closesocket(clientSocket);
+    WSACleanup();
+
+    return 0;
+}
